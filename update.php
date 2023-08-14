3ed77efd880027076fbb89356f891322d5ee6d09
FlaviaAMorgado <127445372+FlaviaAMorgado@users.noreply.github.com>
2023-08-14 08:36:45 -0300

    Update update.php te amo flavinhaa

--- update.php
+++ update.php
@@ -1,7 +1,13 @@
 <?php
 include 'conexao.php';
 
-$$nome = $_POST['txtnome'];
-$senha = $_POST['pssenha'];
-$sexo = $_POST['rdbsexo'];
-$estado = $_POST['estado'];
+$nome = $_POST['txtNome'];
+$senha = $_POST['txtSenha'];
+$sexo = $_POST["rdbSexo"];
+$estado = $_POST["sltEstado"];
+$cd = $_POST["txtCodigo"];
+
+$atualizar = $cn->query("udaqte tbl_Cliente set nm_Cliente='$nome', ds_Senha='$senha', sg_Sexo='$sexo, sg_Estado = '$estado' where cd_Cliente = $cd");
+
+header('location:okalterado.html');
+?>
